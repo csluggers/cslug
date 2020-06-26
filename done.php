01b8690f13ba8d113b514476fae867b777c7f89b
csluggers <57333749+csluggers@users.noreply.github.com>
2020-06-26 00:46:37 -0700

    Add files via upload

---
+++ done.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>SIGN UP!</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="stylin.css" />
+	</head>
+	<body>
+		<p>
+			Welcome <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?> <br />
+			Your telephone number is <?php echo $_POST["phone"]; ?> <br />
+			Your email is <?php echo $_POST["email"]; ?> <br />
+			Your response to marketing emails is <?php echo $_POST["market"]; ?> <br />
+			Your comments are: <?php echo $_POST["comments"]; ?> <br />
+		</p>
+		<p>We will be getting in touch with you soon! Thank you!</p>
+		
+	
+			
+	</body>
+	
+</html>
+			
